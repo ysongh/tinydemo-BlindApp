dbf1e2ac40a107622461c4539e6a04d6fbc9cbd2
You Song Hou <ysongweb3@gmail.com>
2024-06-07 22:40:45 -0400

    feat: add a compare integers Nada program

--- packages/nillion/next-project-programs/src/tinydemo.py
+++ packages/nillion/next-project-programs/src/tinydemo.py
@@ -5,6 +5,6 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    new_int = my_int1 + my_int2
+    new_int = (my_int1 >= my_int2).if_else((my_int1 <= my_int2).if_else(Integer(1), Integer(0)), Integer(0))
 
     return [Output(new_int, "my_output", party1)]
