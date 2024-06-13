3d065f723e49d0eba21bffe323a3b18008f277a2
You Song Hou <ysongweb3@gmail.com>
2024-06-12 21:34:16 -0400

    feat: add a link to the game page

--- packages/nextjs/app/data-list/page.tsx
+++ packages/nextjs/app/data-list/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import type { NextPage } from "next";
+import { useRouter } from "next/navigation";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
   const { data: list } = useScaffoldContractRead({
     contractName: "Tinydemo",
     functionName: "getList",
@@ -23,6 +26,14 @@ const Home: NextPage = () => {
               <div key={index}>
                 <p>Store Id: {l.storeId}</p>
                 <p>Program Id: {l.programId}</p>
+                <button
+                  className={`mt-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                    " bg-indigo-600"
+                  }`}
+                  onClick={() => router.push(`/data-list/${index}`)}
+                >
+                  View
+                </button>
               </div>
             ))}
           </div>
